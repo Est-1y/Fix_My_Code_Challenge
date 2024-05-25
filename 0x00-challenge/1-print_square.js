3ae1568c8506079f1f42399e07733548a2cc18b7
Est-1y <terryiana22@gmail.com>
2024-05-25 10:38:04 +0000

    print square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -23,9 +23,5 @@ if (isNaN(size) || size <= 0) {
 }
 
 for (let i = 0; i < size; i++) {
-    let row = '';
-    for (let j = 0; j < size; j++) {
-        row += '#';
-    }
-    console.log(row);
+    console.log("#".repeat(size));
 }
