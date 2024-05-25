2ecfc524ea74df9b850a43d31978f06fc64df142
Est-1y <terryiana22@gmail.com>
2024-05-25 10:09:10 +0000

    print square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,19 +6,26 @@
     of the program.
 */
 
-
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+    process.exit(1);
 }
 
-size = parseInt(process.argv[2], 10)
+const size = parseInt(process.argv[2], 10);
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
+if (isNaN(size) || size <= 0) {
+    process.stderr.write("Invalid size\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1);
+}
+
+for (let i = 0; i < size; i++) {
+    let row = "";
+    for (let j = 0; j < size; j++) {
+        row += "#";
     }
-    process.stdout.write("\n");
+    console.log(row);
 }
