040e6c38b47d8d779e2474bb82a5283544355532
Est-1y <terryiana22@gmail.com>
2024-05-25 13:32:13 +0000

    print square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
+
 /*
     Print a square with the character #
-    
+
     The size of the square must be the first argument 
     of the program.
 */
-if (process.argv.length <= 2) {
-    process.stderr.write("Missing argument\n");
+
+if (process.argv.length !== 3) {
+    process.stderr.write("Invalid usage: exactly one argument required\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1);
@@ -15,7 +17,7 @@ if (process.argv.length <= 2) {
 const size = parseInt(process.argv[2], 10);
 
 if (isNaN(size) || size <= 0) {
-    process.stderr.write("Invalid size\n");
+    process.stderr.write("Invalid size: size must be a positive integer\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1);
