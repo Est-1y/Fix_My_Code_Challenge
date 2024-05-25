7cb83731db0cb49fc50b5aa5ce7cab605ad30fd8
Est-1y <terryiana22@gmail.com>
2024-05-25 09:22:12 +0000

    fizzbuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -3,7 +3,6 @@
 """
 import sys
 
-
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
@@ -27,7 +26,6 @@ def fizzbuzz(n):
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
 
-
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print("Missing number")
@@ -35,5 +33,17 @@ if __name__ == '__main__':
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
-    number = int(sys.argv[1])
+    try:
+        number = int(sys.argv[1])
+    except ValueError:
+        print("Invalid number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    if number < 1:
+        print("Number must be greater than 0")
+        sys.exit(1)
+
     fizzbuzz(number)
+
